770359ecb948055f3fdd7535d9bdcc6bb44aaca5
samkho-d <dsamk21@freeuni.edu.ge>
2025-06-20 17:04:07 +0400

    Added UserController for logging in and registering

---
+++ src/main/java/ge/edu/freeuni/controller/UserController.java
@@ -0,0 +1,52 @@
+package ge.edu.freeuni.controller;
+
+import ge.edu.freeuni.dao.UserDao;
+import ge.edu.freeuni.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserDao users;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String name, @RequestParam String password) {
+        ModelAndView mav = new ModelAndView("login");
+        if (!users.contains(name)) {
+            mav.addObject("error", "User does not exist.");
+        } else if (!users.correctPassword(name, password)) {
+            mav.addObject("error", "Incorrect password.");
+        }
+        return mav;
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public ModelAndView register(HttpServletResponse response, @RequestParam String name, @RequestParam String password) {
+        ModelAndView mav = new ModelAndView("register");
+        if (!users.add(name, password)) {
+            mav.addObject("error", "User already exists.");
+            return mav;
+        }
+        return new ModelAndView("redirect:/login");
+    }
+
+
+}
